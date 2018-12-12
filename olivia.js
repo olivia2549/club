c764b6bbdbd7286eef9abcdc5001f00634eebc24
olivia2549 <ologanmail@gmail.com>
2018-12-11 22:17:08 -0500

    kjhkj

--- olivia.js
+++ olivia.js
@@ -1,3 +1,12 @@
 function secret() {
   alert("the code is: 123456!")
 }
+
+function myFunction() {
+  var x = document.getElementById("myQuotes");
+  if (x.innerHTML == "'If your ship doesn't come in, swim out to meet it'") {
+    x.innerHTML = "'Another cool quote!'";
+  } else {
+    x.innerHTML = "'If your ship doesn't come in, swim out to meet it'";
+  }
+}
